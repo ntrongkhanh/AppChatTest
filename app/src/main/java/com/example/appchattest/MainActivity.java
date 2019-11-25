1e6bb82a9d970c7213b6930eea46df94babde515
17520720 <50265171+17520720@users.noreply.github.com>
2019-11-25 22:17:30 +0700

    Màn hình đăng kí

--- app/src/main/java/com/example/appchattest/MainActivity.java
+++ app/src/main/java/com/example/appchattest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.appchattest;
 
 import android.content.Intent;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -11,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void SignInWithAnonymous(View view) {
+    }
+
+    public void SignUp(View view) {
+        startActivity(new Intent(MainActivity.this,  CreateAccount.class));
+    }
+
+    public void ContinueToEnterPassword(View view) {
     }
 }
