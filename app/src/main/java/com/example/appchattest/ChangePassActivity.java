d60af04bb3aeedfaf10b9d2c4cc3ad8f38cf0c05
Nguyen Thanh Luan <50266706+17520722@users.noreply.github.com>
2020-01-02 20:12:52 +0700

    Update ChangePassActivity.java
    commit

--- app/src/main/java/com/example/appchattest/ChangePassActivity.java
+++ app/src/main/java/com/example/appchattest/ChangePassActivity.java
@@ -39,6 +39,7 @@ public class ChangePassActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_change_pass);
 
         editTextNewPassword = findViewById(R.id.editText_Change_NewPass);
